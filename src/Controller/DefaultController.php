91956d11bc90109c28cb124f6e15587a99d239c9
Patrick Luca Fazzi <patrick91@live.it>
2018-03-28 18:27:08 +0200

    Default Controller created

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function homeAction()
+    {
+        return new Response('Ciao mondo');
+    }
+
+}
